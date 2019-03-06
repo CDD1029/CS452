517bb917670ae1245c48e0365237b63c958bb746
sorensoo <32225479+sorensoo@users.noreply.github.com>
2019-03-06 14:29:46 -0500

    Update Code.cpp
    issue #4

--- Project 3/Code.cpp
+++ Project 3/Code.cpp
@@ -1,12 +1,52 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "mpi.h" // message passing interface
 using namespace std;
 
 // New compile and run commands for MPI!
 // mpicxx -o blah file.cpp
 // mpirun -q -np 32 blah
+
+void mergesort(int * a, int first, int last);
+void merge(int * a, int * b, int first, int last, int middle);
+
+void mergesort(int * a, int first, int last){
+	if (last > first+1){
+		cout << first << ":" << last << endl;
+		int m = first+(last-first)/2;
+		mergesort(a, first, m);
+		mergesort(a, m, last);
+		int * b = a;
+		merge(a, b, first, last, m);
+	}
+	return;
+}
+
+void merge(int * a, int * b, int first, int last, int middle){
+	int aplace = first;
+	int bplace = middle;
+	while (aplace<middle && bplace<last){
+		if (a[aplace] <= a[bplace])
+			aplace++;
+		else {
+			int value = a[bplace];
+			int index = bplace;
+			
+			while (index>=aplace){
+				a[index] = a[index - 1]; 
+                		index--;
+			}
+			a[aplace] = value;
+			
+			aplace++;
+			bplace++;
+			middle++;
+		}
+	}
+}
 
 int main (int argc, char * argv[]) {
 
@@ -28,8 +68,29 @@ int main (int argc, char * argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	
 	// THE REAL PROGRAM IS HERE
-    cout << my_rank << endl;
-
+	
+	if (my_rank==0){
+		srand(1231);
+		int arraysize=0;
+		cout << "Enter size of array:" << endl;
+		cin >> arraysize;
+		int * array = new int[arraysize];
+		
+		for (int i=0; i<arraysize; i++){
+			array[i]=rand()%10;
+		}
+		cout<< "Unsorted:"<<endl;
+		for (int i=0; i < arraysize; i++){
+			cout<< array[i] << endl;
+		}
+		
+		mergesort(array, 0, arraysize);
+		cout<< "Sorted:"<<endl;
+		for (int i=0; i < arraysize; i++){
+			cout<< array[i] << endl;
+		}
+	}
+	
 	// Shut down MPI
 	MPI_Finalize();
 
